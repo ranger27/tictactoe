44c1c638e2da552a0360c138584d38b1b489ba8d
avisek <avisekksarma@gmail.com>
2021-05-25 19:46:45 +0545

    made somefolders and modified .gitignore file for vscode specific

---
+++ HeaderFiles/board.h
@@ -0,0 +1,4 @@
+#ifndef BOARD_FILE_MEMBERS
+#define BOARD_FILE_MEMBERS
+void sayHi();
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "./HeaderFiles/board.h"
+using namespace std;
+
+int main(){
+    sayHi();
+    return 0;
+}
